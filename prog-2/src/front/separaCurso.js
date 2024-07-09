c2c421440877f436ead0ae6e253c705dcc8e2cb3
Cotoco <brunofneckel123@gmail.com>
2024-07-09 17:18:50 -0300

    removido arquivos lixo

--- prog-2/src/front/separaCurso.js
+++
@@ -1,8 +0,0 @@
-export const separaCurso = (curso) => {
-    return {
-      name: curso.name(),
-      coordenador: curso.coordenador(),
-      descricao: curso.descricao(),
-      turno: curso.turno,
-    };
-  };
